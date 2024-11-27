e18006b7bb6a4142207f0c0bea68edd66a0f3c57
nareshkumar-h <nareshkumarh@live.com>
2024-11-27 12:58:39 +0530

    updated configuration

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css',
   standalone: true
