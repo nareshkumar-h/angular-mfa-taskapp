c291f28f41e48fa55a28551a24f644b72be1aba8
nareshkumar-h <nareshkumarh@live.com>
2024-11-27 12:15:59 +0530

    updated configuration

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css',
   standalone: true
 })
 export class TaskListComponent {
 
-  tasks: any = [{ title: 'Task 1' }, { title: 'Task 2' }]
+  // tasks: any = [{ title: 'Task 1' }, { title: 'Task 2' }]
 }
