58b4e79f751fd1f9a361543a148fa593b13fd354
nareshkumar-h <nareshkumarh@live.com>
2024-11-27 09:58:27 +0530

    updated tasks

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
 
+  tasks: any = [{ title: 'Task 1' }, { title: 'Task 2' }]
 }
